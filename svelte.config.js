3aa1b5646ce17a554c68cacf2be7f1fd511a32d5
Patipon Riebpradit <patipon.riebpradit@gmail.com>
2024-02-13 14:46:40 +0700

    [+] github page setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +8,9 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+    }
   },
 
   preprocess: [vitePreprocess({})],
